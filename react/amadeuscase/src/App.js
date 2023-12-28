d8e2456db78e0666080ac711cd764fa990c95671
furo <furo@furo.com>
2023-12-28 11:54:25 +0300

    bootstrap

--- react/amadeuscase/src/App.js
+++ react/amadeuscase/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Container>
+        <Row>
+          <Col xs={1} md={2} ><h2>küçükte görünmez</h2></Col>
+          <Col xs={10} md={8}><h1>hep görünür</h1></Col>
+          <Col xs={1} md={2}><h2>küçükte görünmez</h2></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
